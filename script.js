95ffda9c4fe13f735c08ab700806f661d21fc96d
Ginger-bread <youngdibz12@outlook.com>
2020-01-31 11:18:35 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+
+const FIRST_NAME = 'Abdoulie'
+const LAST_NAME = 'Dibbasey'
+let fullName = FIRST_NAME +  ' ' + LAST_NAME
+
+const street= 'lungo {dora liguria'
+const housenum= '58'
+const postcode= '73273'
+const city= 'torino(TO)'
+const country = 'italy'
+
+//'lungo dora liguria, 58\n73273 Torino(TO)\nitaly';
+
+const hisGF = 'Amina'
+const myGf = 'Hanna'
+const ourGFs = 'His GF is '+ hisGF + ', While my Gf is '+ myGf
+
+const mylife = 'is hard'
+const hislife = 'is easy'
+const ourlifes = 'his life is ' + hislife + ' ,while my life is ' + mylife
+
+const new_line = "\n"
+const space = " "
+const africa = "the future"
+const europe = "power house"
+const asia = "efficient"
+const atlanta = "city in America"
+ 
+let world = africa +  ' ' + europe + new_line  + africa + ' ' + atlanta + new_line + asia
+
+let admin
+let name = "john"
+admin = name
+alert ( admin ); 
+
+const newAddress = `(${street}, ${housenum} 
+${postcode},
+${city} , ${country},)`
+console.log(newAddress)
+ 
+// const myAddress = '(' +street + ',' + ' '+ housenum + '\n' + postcode + ' ' +city + '\n'+ country + ')'
+
+
+
+let personalinfo = "abdoulie dibbasey, lungo dora liguria, 58 10143 Torino(italia)"
+
+let Professionalexperience = "Logistics, Maggazienere, techExpert, TouristGuide, Artist"
+
+let languages = "English,German,Italian,Mandingo,Wolof"
+
+let digitalcompetenz = "BasicIT,MicrosoftPP,Excel,Word,HTML,Java,CSS"
+
+let Communicationskills = "TeamWorking,TimeManagement,"
+
+let Nationality = "Gambian"
+
+let gender = "Male"
+
+let cv = '(' + personalinfo + ' ' + Professionalexperience +' '+ languages +' ' + digitalcompetenz + ' '+Communicationskills + ''+ Nationality + ''+ gender + ')'
+
+
+const Cv = 
+`${personalinfo },
+ $ {hr}
+ ${Professionalexperience},
+ $ {hr}
+ ${languages},
+ $ {hr} 
+ ${digitalcompetenz}, 
+ $ {hr}
+ ${Communicationskills},
+ $ {hr} 
+ ${Nationality},
+ $ {hr}
+ ${gender}`
+
+
+console.log (cv)
